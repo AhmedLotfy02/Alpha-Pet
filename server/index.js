4d941be2c6fa8bc72274a0ea8ded2eed3a79d64f
waleedhesham446 <waleedhesham2001@gmail.com>
2021-12-08 21:52:35 +0200

    Created the database & Connected to mysql server

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const mysql = require('mysql');
+const dotenv = require('dotenv');
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -12,6 +15,26 @@ app.use('/', (req, res) => {
     res.send('Welcome to Alpha-Pet App');
 });
 
+//  Configuration 'Connection' Object
+const config = {
+    host: 'localhost',
+    user: 'root',
+    password: process.env.MYSQL_SERVER_PASSWORD,           //  My mysql server password
+    database: 'alphapet'
+}
+
+//  Create Connection
+const db = mysql.createConnection(config);
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+//  Connect to MySql server
+db.connect((err) => {
+    if(err){
+        throw err;
+    }else{
+        console.log('MySql Connected...');
+        app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+    }
+});
+
