c17eff38f1336af6c938a29170ba08f8dc144b71
ahmedasad236 <ahmedasad236@gmail.com>
2022-01-06 13:59:02 +0200

    Edit password of localhost

--- server/connection.js
+++ server/connection.js
@@ -6,9 +6,9 @@ dotenv.config();
 const config = {
     host: 'localhost',
     user: 'root',
-   // password: process.env.MYSQL_SERVER_PASSWORD,           //  My mysql server password
-   passowrd:'ahmed',
-   database: 'alpha_pet'
+    password: process.env.MYSQL_SERVER_PASSWORD,           //  My mysql server password
+    
+    database: 'alpha_pet'
 }
 
 //  Create Connection
