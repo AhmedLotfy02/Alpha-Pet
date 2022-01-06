86651dc3baaa781ddf9ad9469d278b8d1129b4dc
waleedhesham446 <waleedhesham2001@gmail.com>
2022-01-06 18:42:21 +0200

    Updated auth

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -2,8 +2,11 @@ const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
     try {
-        const token = req.headers.authorization.split(" ")[1];
-        jwt.verify(token, "this_should_be_very_long");
+        // const token = req.headers.authorization.split(" ")[1];
+		const token = req.headers.authorization.split(" ")[1].slice(0, -1);	//	to remove the " in the last character in the string
+        // jwt.verify(token, "this_should_be_very_long");
+		const decoded = jwt.verify(token, "this_should_be_very_long");
+        req.body.currentUserEmail = decoded.email;						//	used in the the functions
         next();
     } catch (error) {
         res.status(401).json({ message: "Auth failed!" });
